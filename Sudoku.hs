0820a9a0dded6bd31d535c8722e3bf353502055e
mark on github <markongithub@users.noreply.github.com>
2018-08-24 23:02:59 +0000

    OK this works so commit before I break it

--- Sudoku.hs
+++ Sudoku.hs
@@ -54,7 +54,7 @@ allNeighbors :: RowCol -> [RowCol]
 allNeighbors rc = concat [rowLeft rc, rowRight rc, columnUp rc, columnDown rc,
                           boxMates rc]
 
-eliminatePossibility :: Int -> Square -> Square
+eliminatePossibility :: Possibility -> Square -> Square
 eliminatePossibility i rc = case rc of
   Known k -> if (k == i)
                then error ("I tried to eliminate " ++ (show i) ++
@@ -101,29 +101,45 @@ readDigitOrCrash c = let
 
 charToSquare :: Char -> Square
 charToSquare ' ' = unknown
+charToSquare '0' = unknown
+charToSquare '.' = unknown
 charToSquare c = Known (readDigitOrCrash c)
 
 -- https://www.websudoku.com/?level=4&set_id=3381457023
-testPuzzleRows = [" 3   1 9 ",
-                  " 982     ",
-                  " 5 8  7  ",
-                  " 24 8    ",
-                  "  3   5  ",
-                  "    3 17 ",
-                  "  7  9 1 ",
-                  "     648 ",
-                  " 4 5   6 "]
+--testPuzzleRows = [" 3   1 9 ",
+--                  " 982     ",
+--                  " 5 8  7  ",
+--                  " 24 8    ",
+--                  "  3   5  ",
+--                  "    3 17 ",
+--                  "  7  9 1 ",
+--                  "     648 ",
+--                  " 4 5   6 "]
+testPuzzleRows2 = ["  46     ",
+                   "  32 9   ",
+                   " 12 4   8",
+                   "      9 4",
+                   "4   8   6",
+                   "1 9      ",
+                   "7   3 21 ",
+                   "   7 28  ",
+                   "     15  "]
 
-testBoard = parseRows testPuzzleRows
-testProgress = snd $ iterateUntilStable reduceFromAllSquares testBoard
+testPuzzleRows = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+
+testBoard = parseBoard testPuzzleRows
+testProgress = snd $ iterateUntilStable performAllOperations testBoard
 
 parseRows :: [String] -> Board
-parseRows rows = let
-  oneBigString = concat rows
+parseRows rows = parseBoard $ concat rows
+
+parseBoard :: String -> Board
+parseBoard oneBigString = let
   squares = map charToSquare oneBigString
   pairs:: [(Int, Square)]
   pairs = zip [0..80] squares
   in array (0,80) pairs
+
 
 fixSinglePossibility :: Square -> Square
 fixSinglePossibility sq = case sq of
@@ -134,12 +150,16 @@ reduceFromSquare :: Board -> Int -> Board
 reduceFromSquare b i = let
   newBoard = b//[(i, fixSinglePossibility (b!i))]
   neighbors = map rowColIndex $ allNeighbors $ indexRowCol i
-  makeUpdate :: Int -> Int -> (Int, Square)
-  makeUpdate v j = (j, eliminatePossibility v (newBoard!j))
-  allUpdates v = map (makeUpdate v) neighbors
   in case newBoard!i of
-    Known knownValue -> newBoard//(allUpdates knownValue)
+    Known knownValue -> eliminateFromIndices newBoard knownValue neighbors
     _                -> newBoard
+
+eliminateFromIndices :: Board -> Possibility -> [Index] -> Board
+eliminateFromIndices board possibility indices = let
+  makeUpdate :: Index -> (Index, Square)
+  makeUpdate i = (i, eliminatePossibility possibility (board!i))
+  allUpdates = map makeUpdate indices
+  in board//allUpdates
 
 reduceFromAllSquares :: Board -> Board
 reduceFromAllSquares b = foldl reduceFromSquare b [0..80]
@@ -159,32 +179,64 @@ indicesForBox b = let
   in [ 9 * x + y + upperLeft | x<-[0..2], y<-[0..2] ]
 -- 0 -> 0-2,9-11,18-20
 
+indicesForHouse :: HouseID -> [Int]
+indicesForHouse (HouseID Row r) = indicesForRow r
+indicesForHouse (HouseID Column c) = indicesForColumn c
+indicesForHouse (HouseID Box b) = indicesForBox b
+
+allHouses :: [HouseID]
+allHouses = [ (HouseID hType x) | hType<-[Row, Column, Box], x<-[0..8]]
+
 boxForIndex :: Index -> Int
 boxForIndex i = let
   RowCol r c = indexRowCol i
-  in (r `div` 3) + (c `div` 3)
+  in 3 * (r `div` 3) + (c `div` 3)
 
 allIndexLists :: [[Int]]
 allIndexLists = [ f list | f<-[indicesForRow, indicesForColumn, indicesForBox], list<-[0..8]]
 
 type DigitsToIndices = Map Possibility (Set Index)
-type DigitSetsToIndices = Map (Set Possibility) (Set Index)
+type IndexSetUnions = Map (Set Index) (Set Possibility)
 
+data HouseType = Row | Column | Box deriving (Eq, Show)
 data NineSquareData = NineSquareData { digitsToIndices :: DigitsToIndices
-                                     , digitSetsToIndices :: DigitSetsToIndices } deriving (Eq, Show)
+                                     , indexSetUnions :: IndexSetUnions
+                                     , house :: HouseID } deriving (Eq, Show)
+
+data HouseID = HouseID HouseType Int deriving (Eq, Show)
 
 addPossibilityToTable :: DigitsToIndices -> (Int, Int) -> DigitsToIndices
 addPossibilityToTable t (index, possibility) = Map.insertWith Set.union possibility (Set.singleton index) t
 
-updateDigitSetsToIndices :: DigitSetsToIndices -> Set Possibility -> Index -> DigitSetsToIndices
-updateDigitSetsToIndices t possibilities index = Map.insertWith Set.union possibilities (Set.singleton index) t
+-- updateIndexSetUnions :: IndexSetUnions -> Set Possibility -> Index -> IndexSetUnions
+-- updateIndexSetUnions t possibilities index = Map.insertWith Set.union possibilities (Set.singleton index) t
+
+-- get rid of this when we get containers 0.5.11 or later
+powerSet :: Ord a => Set a -> Set (Set a)
+powerSet set = let
+  min = Set.findMin set
+  withoutMin = powerSet $ Set.deleteMin set
+  withMin = Set.map (Set.insert min) withoutMin
+  fancySubsets = Set.union withoutMin withMin
+  in if Set.null set then (Set.singleton Set.empty) else fancySubsets
 
 addPossibilitySetToTables :: NineSquareData -> Int -> Set Int -> NineSquareData
-addPossibilitySetToTables (NineSquareData oldPTable oldSetTable) index possibilities = let
+addPossibilitySetToTables (NineSquareData oldPTable oldIndexSets house) index possibilities = let
   dPairs = zip (repeat index) (Set.toList possibilities)
   newDigits = foldl addPossibilityToTable oldPTable dPairs
-  newSets = updateDigitSetsToIndices oldSetTable possibilities index
-  in NineSquareData newDigits newSets
+  newIndexSets = updateIndexSetUnions oldIndexSets index possibilities
+  in NineSquareData newDigits newIndexSets house
+
+updateIndexSetUnions :: IndexSetUnions -> Int -> Set Int -> IndexSetUnions
+updateIndexSetUnions oldISU index possibilities = let
+  oldPairs :: [(Set Index, Set Possibility)]
+  oldPairs = Map.toList oldISU
+  newSingletonPair = (Set.singleton index, possibilities)
+  makeNewSet :: (Set Index, Set Possibility) -> (Set Index, Set Possibility)
+  makeNewSet (oldI, oldP) = (Set.insert index oldI, Set.union possibilities oldP)
+  newPairs = map makeNewSet oldPairs
+  newISU = Map.fromList (newSingletonPair:newPairs)
+  in Map.unionWith (error "duplicate key") oldISU newISU
  
 addToTableFromBoardSquare :: Board -> NineSquareData -> Index -> NineSquareData
 addToTableFromBoardSquare board table index = case board!index of
@@ -193,21 +245,68 @@ addToTableFromBoardSquare board table index = case board!index of
 
 makeTableFromBoardSquares :: Board -> [Index] -> NineSquareData
 makeTableFromBoardSquares board indices = let
-  emptyNSD = NineSquareData Map.empty Map.empty
+  emptyNSD = NineSquareData Map.empty Map.empty undefined
+  in foldl (addToTableFromBoardSquare board) emptyNSD indices
+
+makeTableFromHouse :: Board -> HouseID -> NineSquareData
+makeTableFromHouse board house = let
+  indices = indicesForHouse house
+  emptyNSD = NineSquareData Map.empty Map.empty house
   in foldl (addToTableFromBoardSquare board) emptyNSD indices
 
 iterateUntilStable0 f x i = let
   output = f x
   in if (output == x) then (i, x) else (iterateUntilStable0 f output (i + 1))
 
-updateBoardUsingEntry :: Board -> (Int, Set Int) -> Board
-updateBoardUsingEntry b (possibility, set) = case (Set.size set) of
+updateBoardUsingIndexSet :: HouseID -> Board -> (Set Index, Set Possibility) -> Board
+updateBoardUsingIndexSet house board (indices, possibilities) = let
+  indicesToUpdate = filter (\i -> Set.notMember i indices) $ indicesForHouse house
+  eliminate :: Board -> Possibility -> Board
+  eliminate b p = eliminateFromIndices b p indicesToUpdate
+  eliminateAll = foldl eliminate board (Set.toList possibilities)
+  canEliminate = Set.size indices == Set.size possibilities
+  in if canEliminate then eliminateAll else board
+
+updateBoardUsingEntry :: HouseID -> Board -> (Possibility, Set Index) -> Board
+updateBoardUsingEntry house b (possibility, set) = case (Set.size set) of
   0 -> error "How did I get a set of size zero here?"
   1 -> reduceFromSquare (b//[(Set.findMin set, Known possibility)]) (Set.findMin set)
-  _ -> b
+  _ -> reduceLockedCandidates b (possibility, set) house
 
+reduceLockedCandidates :: Board -> (Possibility, Set Index) -> HouseID -> Board
+reduceLockedCandidates b (possibility, set) house = case (findLockedCandidatesFromHouse house $ Set.toList set) of
+  Just house -> eliminatePossibilityFromLockedCandidates b possibility set house
+  Nothing    -> b
+
+eliminatePossibilityFromLockedCandidates :: Board -> Possibility -> Set Index -> HouseID -> Board
+eliminatePossibilityFromLockedCandidates b p s h = let
+  indicesToUpdate = filter (\i -> Set.notMember i s) $ indicesForHouse h
+  in eliminateFromIndices b p indicesToUpdate
+
+findLockedCandidatesFromHouse :: HouseID -> [Index] -> Maybe HouseID
+findLockedCandidatesFromHouse (HouseID hType _) indices = case hType of
+  Box -> indicesAreSameRowOrColumn indices
+  _   -> indicesAreSameBox indices
+  
 updateBoardUsingTable :: Board -> NineSquareData -> Board
-updateBoardUsingTable board (NineSquareData dTable _) = foldl updateBoardUsingEntry board (Map.toList dTable)
+updateBoardUsingTable board (NineSquareData dTable indexSets h) = let
+  afterHiddenSingles = foldl (updateBoardUsingEntry h) board (Map.toList dTable)
+  afterIndexSets = foldl (updateBoardUsingIndexSet h) afterHiddenSingles (Map.toList indexSets)
+  in afterIndexSets
+
+-- updateBoardFromLockedCandidates :: Board -> NineSquareData -> Board
+-- updateBoardFromLockedCandidates board (NineSquareData dTable _ hType) = let
+
+-- what can you do with a locked candidate
+-- you would need the list of indices to affect
+-- and the list of indices NOT to affect
+-- and the possibility to eliminate
+-- so if you have a box and you notice all the 2s are in column 4
+-- you want to eliminate all the 2s in column 4 EXCEPT THOSE INDICES
+-- so you would have [(2,fromList [58,67] and (2,Just 4) and Column?
+-- then you'd figure out all the indices in column 4 minus [58,67]
+-- and eliminate the 2s from those indices using eliminateFromIndices
+
 
 updateBoardUsingIndices :: Board -> [Index] -> Board
 updateBoardUsingIndices board indices = updateBoardUsingTable board (makeTableFromBoardSquares board indices)
@@ -215,23 +314,37 @@ updateBoardUsingIndices board indices = updateBoardUsingTable board (makeTableFr
 updateBoardUsingAllHiddenSingles :: Board -> Board
 updateBoardUsingAllHiddenSingles board = foldl updateBoardUsingIndices board allIndexLists
 
-performAllOperations :: Board -> Board
-performAllOperations = reduceFromAllSquares . updateBoardUsingAllHiddenSingles
+updateBoardUsingHouse :: Board -> HouseID -> Board
+updateBoardUsingHouse board house = updateBoardUsingTable board (makeTableFromHouse board house)
 
-indicesAreSameRow :: [Int] -> Maybe Int
+updateBoardUsingAllHouses :: Board -> Board
+updateBoardUsingAllHouses board = foldl updateBoardUsingHouse board allHouses
+
+performAllOperations :: Board -> Board
+performAllOperations = reduceFromAllSquares . updateBoardUsingAllHouses
+
+indicesAreSameRow :: [Int] -> Maybe HouseID
 indicesAreSameRow [] = error "indicesAreSameRow on empty list"
-indicesAreSameRow [i] = Just (i `div` 9)
+indicesAreSameRow [i] = Just $ HouseID Row (i `div` 9)
 indicesAreSameRow (x:y:xs) = if (x `div` 9 == y `div` 9) then indicesAreSameRow (y:xs) else Nothing
 
-indicesAreSameColumn :: [Int] -> Maybe Int
+indicesAreSameColumn :: [Int] -> Maybe HouseID
 indicesAreSameColumn [] = error "indicesAreSameColumn on empty list"
-indicesAreSameColumn [i] = Just (i `mod` 9)
+indicesAreSameColumn [i] = Just $ HouseID Column (i `mod` 9)
 indicesAreSameColumn (x:y:xs) = if (x `mod` 9 == y `mod` 9) then indicesAreSameColumn (y:xs) else Nothing
 
-indicesAreSameBox :: [Int] -> Maybe Int
+indicesAreSameBox :: [Int] -> Maybe HouseID
 indicesAreSameBox [] = error "indicesAreSameBox on empty list"
 indicesAreSameBox indices = let
   (x:xs) = map boxForIndex indices
-  in if (all (== x) xs) then Just x else Nothing
+  in if (all (== x) xs) then Just (HouseID Box x) else Nothing
 
+--indicesAreSameHouse :: HouseType -> [Int] -> Maybe HouseID
+--indicesAreSameHouse Row = indicesAreSameRow
+--indicesAreSameHouse Column = indicesAreSameColumn
+--indicesAreSameHouse Box = indicesAreSameBox
 
+indicesAreSameRowOrColumn :: [Int] -> Maybe HouseID
+indicesAreSameRowOrColumn indices = case indicesAreSameRow indices of
+  Just r -> Just r
+  Nothing -> indicesAreSameColumn indices
